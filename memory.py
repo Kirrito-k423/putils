8202e456ff4e149efd90a550a160d1d5ed487260
Your Name <you@example.com>
2025-04-30 14:25:41 +0800

    [feat] memand profiling

---
+++ memory.py
@@ -0,0 +1,77 @@
+from megatron.training.utils import report_memory
+import torch
+from contextlib import contextmanager
+
+def pmem_str():
+    torch.npu.synchronize()
+    mega_bytes = 1024.0 * 1024.0 * 1024.0
+    string = ' | allocated: {}'.format(
+        torch.cuda.memory_allocated() / mega_bytes)
+    string += ' | max allocated: {}'.format(
+        torch.cuda.max_memory_allocated() / mega_bytes)
+    string += ' | reserved: {}'.format(
+        torch.cuda.memory_reserved() / mega_bytes)
+    string += ' | max reserved: {}'.format(
+        torch.cuda.max_memory_reserved() / mega_bytes)
+    return string
+
+
+# Usage:
+# with pmemory("fun xxx"):
+#     funx()
+@contextmanager
+def pmemory(tmp_str):
+    tmp = f"TSJPMEM {tmp_str}"
+    mem_str = pmem_str()
+    with torch.profiler.record_function(tmp+mem_str):
+        report_memory(f"before {tmp}")
+        yield
+        report_memory(f"after {tmp}")
+
+
+
+# def get_tensor_size(param: torch.Tensor) -> int:
+#     """计算张量的显存占用（字节）"""
+#     dtype_size_map = {
+#         torch.float32: 4,
+#         torch.float: 4,
+#         torch.float16: 2,
+#         torch.half: 2,
+#         torch.bfloat16: 2,
+#         torch.int64: 8,
+#         torch.long: 8,
+#         torch.int32: 4,
+#         torch.int: 4,
+#         torch.int16: 2,
+#         torch.int8: 1,
+#         torch.uint8: 1,
+#         torch.bool: 1,
+#     }
+#     element_size = dtype_size_map.get(param.dtype, 4)  # 默认按4字节处理
+#     return param.numel() * element_size  # 总字节数 = 元素数量 × 单元素字节数
+
+def get_tensor_size(tensor):
+    """返回 tensor 的内存大小，单位为 GB"""
+    return tensor.numel() * tensor.element_size() / (1024 ** 3)
+
+# print model size
+def pmsize(model):
+    if torch.distributed.get_rank() == 0:
+        print(f"world size is {torch.distributed.get_world_size()}")
+        for name, module in model.named_modules():
+            num_params = 0
+            total_size = 0
+            print(f"\nLayer detail")
+            for param_name, param in module.named_parameters():
+                num_params += param.numel()
+                tmp_size = get_tensor_size(param)
+                total_size += tmp_size
+                print(f"    {param_name}: shape={tuple(param.shape)} | dtype={param.dtype} | device={param.device} | size {tmp_size} GB")
+            break
+        for name, module in model.named_modules():
+            total_size = 0
+            for param_name, param in module.named_parameters():
+                # tmp_size = get_tensor_size(param) / (1024 ** 3)
+                tmp_size = get_tensor_size(param)
+                total_size += tmp_size
+            print(f"layer {name} {total_size} GB")
---
+++ profiling.py
@@ -0,0 +1,53 @@
+import time
+import os
+import torch
+import torch_npu
+from contextlib import contextmanager
+
+# Usage:
+# export MM_ENABLE_PROFILING=1
+# with profiling_this(True):
+#     funx()
+
+
+@contextmanager
+def profiling_this(encoder_count):
+    from datetime import datetime
+
+    # 获取当前时间
+    current_time = datetime.now()
+
+    # 格式化为可读字符串（例如：2024-07-15 14:30:45）
+    readable_time = current_time.strftime("%Y-%m-%d_%H_%M")
+    profiling_path = os.environ.get("MM_ENABLE_PROFILING_PATH", f"/home/t00906153/profiling/{readable_time}")
+    torch.npu.synchronize()
+    start_time = time.time()
+    if int(os.environ.get("MM_ENABLE_PROFILING", "0")) == 0:
+        yield
+    else:
+        if encoder_count:
+            experimental_config = torch_npu.profiler._ExperimentalConfig(
+                aic_metrics=torch_npu.profiler.AiCMetrics.PipeUtilization,
+                profiler_level=torch_npu.profiler.ProfilerLevel.Level1,
+                data_simplification=False
+            )
+            with torch_npu.profiler.profile(
+                activities=[
+                    torch_npu.profiler.ProfilerActivity.CPU,  # CPU侧python调用
+                    torch_npu.profiler.ProfilerActivity.NPU],
+                with_stack=True,
+                record_shapes=True,
+                profile_memory=True,
+                with_modules=True, # CPU侧python调用
+                schedule=torch_npu.profiler.schedule(wait=0, warmup=0, active=1, repeat=1, skip_first=0),
+                on_trace_ready=torch_npu.profiler.tensorboard_trace_handler(profiling_path),
+                experimental_config=experimental_config
+            ):
+
+                yield  # 在此处执行目标代码行
+        else:
+            yield
+    torch.npu.synchronize()
+    end_time = time.time()
+    run_time = end_time - start_time
+    print(f"time cost is: {run_time * 1000:.2f} ms")
