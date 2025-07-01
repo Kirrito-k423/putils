0b3a83b7274cc14825ca10fa8f21cdaed6513c1c
Your Name <you@example.com>
2025-07-01 14:24:27 +0800

    smarter print

--- pprint.py
+++ pprint.py
@@ -16,7 +16,8 @@ def log_or_print(content):
 def ifdebug():
     return int(os.environ.get("TSJPRINT", 0)) == 1
 
-def tensor_md5(tensor: torch.Tensor) -> str:
+
+def tensor_md5(tensor: torch.Tensor, length=6) -> str:
     if tensor.dtype == torch.bfloat16:
         tensor = tensor.float()  # BFloat16 → Float32
     # 确保 Tensor 是连续的，并转为 numpy 数组
@@ -24,7 +25,7 @@ def tensor_md5(tensor: torch.Tensor) -> str:
     # 生成字节流
     tensor_bytes = tensor_np.tobytes()
     # 计算 MD5
-    return hashlib.md5(tensor_bytes).hexdigest()
+    return hashlib.md5(tensor_bytes).hexdigest()[:length]
 
 def aprint(name, tensors,file_path=''):
     if ifdebug():
@@ -46,7 +47,8 @@ def tprint(obj, name=""):
             except Exception as e:
                 tmp_mean = e
             mem_size=obj.element_size() * obj.numel() / 1024**3
-            tmp_str1= f"'{name}'| {origin_dtype} | mean {tmp_mean} | sum {obj.sum()} | Shape: {obj.shape} | hash: {tensor_md5(obj)} | Size {obj.numel()} | Memory size: {mem_size:.2f} GB | isNan {torch.isnan(obj).any()} "
+            tmp_str1= f"'{name}'| {tensor_md5(obj)} | {origin_dtype} | {obj.shape} | continue: {obj.is_contiguous()} | mean {tmp_mean} | sum {obj.sum()} "
+            tmp_str1+= f"| Size {obj.numel()} | Memory size: {mem_size:.2f} GB | isNan {torch.isnan(obj).any()}"
             print(tmp_str1, flush=True)
             tmp_str2=obj.flatten()[:10].tolist()
             print(tmp_str2, flush=True)
