72b65898d87c9dd23a1bd2944c44c8664e4303dc
shaojiemike <shaojiemike@mail.ustc.edu.cn>
2025-07-19 09:57:35 +0800

    gdb listen all processes

--- debug.py
+++ debug.py
@@ -8,17 +8,28 @@ import portalocker
 import socket
 import os
 import debugpy# 防止多次调用 listen()
-def vscode_gdb(port=23325, lock_file="/tmp/debugpy.lock"):
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+        
+def vscode_gdb(start_port=23325, end_port=24000,lock_file="/tmp/debugpy.lock"):
     if not ifdebug():
         return
 
-    # Step 1: 检查端口是否真的被占用
-    def is_port_in_use(port):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            return s.connect_ex(('localhost', port)) == 0
+    # Step 1: 找到一个可用端口
+    selected_port = None
+    for port in range(start_port, end_port + 1):
+        if not is_port_in_use(port):
+            selected_port = port
+            break
+        else:
+            print(f"🔒 Port {port} already in use. Check next port...")
 
-    if is_port_in_use(port):
-        print(f"🔒 Port {port} already in use. Skipping debug setup.")
+
+    if selected_port is None:
+        print("❌ No available port found in range.")
+        lock_fd.close()
         return
 
     # Step 2: 尝试获取文件锁，只有获取到锁的进程才绑定端口
@@ -32,16 +43,16 @@ def vscode_gdb(port=23325, lock_file="/tmp/debugpy.lock"):
         portalocker.lock(lock_fd, portalocker.LOCK_EX | portalocker.LOCK_NB)
 
         # 成功获取锁，说明我们可以绑定端口
-        debugpy.listen(("0.0.0.0", port))
-        print(f"⏳ Waiting for debugger to attach on port {port}...")
+        debugpy.listen(("0.0.0.0", selected_port))
+        print(f"⏳ Waiting for debugger to attach on port {selected_port}...",flush=True)
         debugpy.wait_for_client()
-        print("🎉 Debugger attached!")
+        print(f"🎉 Debugger attached on port {selected_port}!")
 
         # 锁文件保持打开状态，进程退出时自动释放锁
         # 可以在这里做其他任务
     except (portalocker.exceptions.LockException, OSError):
         # 获取锁失败，说明其他进程已经绑定端口
-        print(f"🔒 Another process is managing the debug port {port}.")
+        print(f"🔒 Another process is managing the debug port {selected_port}.")
         lock_fd.close()
 # {
 #     // 使用 IntelliSense 了解相关属性。 
