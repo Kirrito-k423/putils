926960dc2ea7f9d6270422813e1236df66d395aa
shaojieMike <shaojiemike@mail.ustc.edu.cn>
2025-09-01 15:50:26 +0800

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,5 +1,6 @@
 import time
 from contextlib import contextmanager
+from .write2file import log2file
 
 # 全局开关，用于控制是否输出耗时信息
 TIMER_VERBOSE = True
@@ -11,6 +12,8 @@ def timer(name=""):
     if TIMER_VERBOSE:
         start = time.perf_counter()
         print(f"[Timer] '{name}' started...")
+        prefix_str = f"{name} func started"
+        log2file(f"{name} {mprint(prefix_str)}")
     try:
         yield
     finally:
@@ -18,6 +21,9 @@ def timer(name=""):
             end = time.perf_counter()
             duration = end - start
             print(f"[Timer] '{name}' finished in {duration:.6f} seconds.")
+            prefix_str = f"{name} func end"
+            log2file(f"{name} {mprint(prefix_str)} during {duration}")
+
 
 
 # 如果你想用装饰器方式统计整个函数
@@ -40,10 +46,16 @@ def to_filename_safe(timestamp_str):
                    .replace(".", "_ms")
     )
 
-def mprint(msg, level="INFO"):
+def get_formatted_time():
     now = time.time()
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
     milliseconds = int((now * 1000) % 1000)
-    formatted_time = f"{timestamp}.{milliseconds:03d}"
+    return f"{timestamp}.{milliseconds:03d}"
+
+def get_time_str():
+    return to_filename_safe(get_formatted_time())
+
+def mprint(msg, level="INFO"):
+    formatted_time = get_formatted_time()
     print(f"[{formatted_time}] [{level}] {msg}")
     return to_filename_safe(formatted_time)
