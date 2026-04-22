b9036f6e1c0999e7820961d555c60d71ea95a12c
shaojiemike <shaojiemike@mail.ustc.edu.cn>
2026-04-22 15:00:18 +0800

    feat: tprint增加max和min值输出

--- putils/pprint.py
+++ putils/pprint.py
@@ -57,8 +57,16 @@ def tprint(obj, name=""):
                 tmp_l1_norm = obj.abs().sum()
             except Exception as e:
                 tmp_l1_norm = e
+            try:
+                tmp_max = obj.max()
+            except Exception as e:
+                tmp_max = e
+            try:
+                tmp_min = obj.min()
+            except Exception as e:
+                tmp_min = e
             mem_size=obj.element_size() * obj.numel() / 1024**3
-            tmp_str1= f"'{name}'| l1_norm {tmp_l1_norm} | {tensor_md5(obj)} | {origin_dtype} | {obj.shape} | continue: {obj.is_contiguous()} | mean {tmp_mean} | sum {obj.sum()} "
+            tmp_str1= f"'{name}'| l1_norm {tmp_l1_norm} | {tensor_md5(obj)} | {origin_dtype} | {obj.shape} | continue: {obj.is_contiguous()} | mean {tmp_mean} | sum {obj.sum()} | max {tmp_max} | min {tmp_min} "
             tmp_str1+= f"| Size {obj.numel()} | Memory size: {mem_size:.2f} GB | isNan {torch.isnan(obj).any()}"
             log_and_print(tmp_str1)
             tmp_str2=obj.flatten()[:10].tolist()
